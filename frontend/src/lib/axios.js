cad04bb759cd0f4254b7d8c22b837ffbd2ac2938
prakharDvedi <prakhardwivedi.12@gmail.com>
2025-12-23 19:19:19 +0530

    corrected APi URL

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_URL
-    ? `${import.meta.env.VITE_API_URL}/api`
+    ? `${import.meta.env.VITE_API_URL.replace(/\/$/, "")}/api`
     : import.meta.env.MODE === "development"
     ? "http://localhost:3000/api"
     : "/api",
